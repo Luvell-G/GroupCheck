dcd8db701fa4378105fc6068be52b77318c4b7ea
Luvell-G <trippmc16@yahoo.com>
2026-04-23 14:04:15 +0000

    Add project files from zip

---
+++ build/CMakeFiles/groupchat_shared.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/data/GroupChat/client/chat_client.cpp" "CMakeFiles/groupchat_shared.dir/client/chat_client.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/client/chat_client.cpp.o.d"
+  "/mnt/data/GroupChat/server/cache.cpp" "CMakeFiles/groupchat_shared.dir/server/cache.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/server/cache.cpp.o.d"
+  "/mnt/data/GroupChat/server/chat_server.cpp" "CMakeFiles/groupchat_shared.dir/server/chat_server.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/server/chat_server.cpp.o.d"
+  "/mnt/data/GroupChat/server/group_manager.cpp" "CMakeFiles/groupchat_shared.dir/server/group_manager.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/server/group_manager.cpp.o.d"
+  "/mnt/data/GroupChat/server/memory_pager.cpp" "CMakeFiles/groupchat_shared.dir/server/memory_pager.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/server/memory_pager.cpp.o.d"
+  "/mnt/data/GroupChat/server/thread_pool.cpp" "CMakeFiles/groupchat_shared.dir/server/thread_pool.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/server/thread_pool.cpp.o.d"
+  "/mnt/data/GroupChat/shared/logger.cpp" "CMakeFiles/groupchat_shared.dir/shared/logger.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/shared/logger.cpp.o.d"
+  "/mnt/data/GroupChat/shared/metrics.cpp" "CMakeFiles/groupchat_shared.dir/shared/metrics.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/shared/metrics.cpp.o.d"
+  "/mnt/data/GroupChat/shared/protocol.cpp" "CMakeFiles/groupchat_shared.dir/shared/protocol.cpp.o" "gcc" "CMakeFiles/groupchat_shared.dir/shared/protocol.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/groupchat_shared.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/groupchat_shared.dir/client/chat_client.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/client/chat_client.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/server/cache.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/server/cache.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/server/chat_server.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/server/chat_server.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/server/group_manager.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/server/group_manager.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/server/memory_pager.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/server/memory_pager.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/server/thread_pool.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/server/thread_pool.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/shared/logger.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/shared/logger.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/shared/metrics.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/shared/metrics.cpp.o.d"
+  "CMakeFiles/groupchat_shared.dir/shared/protocol.cpp.o"
+  "CMakeFiles/groupchat_shared.dir/shared/protocol.cpp.o.d"
+  "libgroupchat_shared.a"
+  "libgroupchat_shared.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/groupchat_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
